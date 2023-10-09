83a531e9b49bad9ba84e6b95a1013d36ff059af7
Jpcapunay <jeanpierrecapunay17@hotmail.com>
2023-10-09 17:08:08 -0400

    rick and morty

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ fetch(url)
       return res.json()
     })
     .then((res)=>{
+      console.log(res.results)
       displayImages(res.results)
     })
     .catch(err => console.log('oops something went wrong', err));
@@ -14,11 +15,41 @@ fetch(url)
 function displayImages(characters){
   characters.forEach(character=>{
     let characterHTML = `
-      <div class="grid-item">
-        <img src="${character.image}" alt="${character.name}"/>
-      </div>
-    `
+  <div class="grid-item">
+    <img src="${character.image}" alt="${character.name}"/>
+    <button class="info-btn" data-name="${character.name}" data-status="${character.status}" data-species="${character.species}">More Info</button>
+  </div>
+`
 
+    document.querySelectorAll('.info-btn').forEach(btn => {
+      btn.addEventListener('click', displayModal);
+    });
+    function displayModal(e){
+      const btn = e.currentTarget;
+      const name = btn.getAttribute('data-name');
+      const status = btn.getAttribute('data-status');
+      const species = btn.getAttribute('data-species');
+      
+      document.getElementById('modal-text').innerText = `${name}\nStatus: ${status}\nSpecies: ${species}`;
+      document.getElementById('myModal').style.display = "block";
+    }
+    
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+    
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+      document.getElementById('myModal').style.display = "none";
+    }
+    
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+      if (event.target === document.getElementById('myModal')) {
+        document.getElementById('myModal').style.display = "none";
+      }
+    }
+    
+     
     container.insertAdjacentHTML("beforeend", characterHTML)
   })
 }
