53f277c73d2f39de1c39cf114bd894f194534361
Jpcapunay <jeanpierrecapunay17@hotmail.com>
2023-10-10 09:13:50 -0400

    completed

--- script.js
+++ script.js
@@ -34,15 +34,14 @@ function displayImages(characters){
       document.getElementById('myModal').style.display = "block";
     }
     
-    // Get the <span> element that closes the modal
+  
     var span = document.getElementsByClassName("close")[0];
     
-    // When the user clicks on <span> (x), close the modal
     span.onclick = function() {
       document.getElementById('myModal').style.display = "none";
     }
     
-    // When the user clicks anywhere outside of the modal, close it
+  
     window.onclick = function(event) {
       if (event.target === document.getElementById('myModal')) {
         document.getElementById('myModal').style.display = "none";
