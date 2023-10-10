b3feec938579d0bec15614e8e67eec943289b8ae
Jpcapunay <jeanpierrecapunay17@hotmail.com>
2023-10-10 09:54:09 -0400

    completed

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const url = 'https://rickandmortyapi.com/api/character'
 const container = document.querySelector(".grid-container")
-
+//Fetch API 
 fetch(url)
     .then(res => {
       return res.json()
@@ -11,7 +11,8 @@ fetch(url)
     })
     .catch(err => console.log('oops something went wrong', err));
 
-
+//function tab takes an array of charcaters as an input.
+//Loops over each character in the array and for each charcater it creates a string of HTML including charcater images,name, status and species.
 function displayImages(characters){
   characters.forEach(character=>{
     let characterHTML = `
@@ -34,14 +35,14 @@ function displayImages(characters){
       document.getElementById('myModal').style.display = "block";
     }
     
-  
+  // get the <span> element that closes the modal
     var span = document.getElementsByClassName("close")[0];
-    
+  //when the user clicks on <span> (x) , close the modal 
     span.onclick = function() {
       document.getElementById('myModal').style.display = "none";
     }
     
-  
+  //when the user clicks anywhere outside of the modal,close it
     window.onclick = function(event) {
       if (event.target === document.getElementById('myModal')) {
         document.getElementById('myModal').style.display = "none";
